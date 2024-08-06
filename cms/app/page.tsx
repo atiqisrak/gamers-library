71413006b4f050637b547fc2c4b6672e7559479f
Niloy <atiqisrak@gmail.com>
2024-08-06 12:54:16 +0600

    initialized cms sidebar

---
+++ cms/app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <center>
+        <h1>Welcome to ShadCN</h1>
+        <Button>Click me</Button>
+      </center>
+    </main>
+  );
+}
---
+++ cms/components/Sidebar.tsx
@@ -0,0 +1,7 @@
+export default function Sidebar() {
+    return (
+        <div
+        className="w-64 h-screen bg-white border-r border-gray-200"
+        >Sidebar</div>
+    );
+}
